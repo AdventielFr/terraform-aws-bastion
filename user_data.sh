7b18ffca9a7cd78c560a7feaa27297b6dd6a2554
iouattara <105317891+iouattara@users.noreply.github.com>
2022-09-21 10:43:03 +0200

    feat: add ssm agent install

--- user_data.sh
+++ user_data.sh
@@ -175,3 +175,12 @@ cat > ~/mycron << EOF
 EOF
 crontab ~/mycron
 rm ~/mycron
+
+#######################
+## INSTALL SSM AGENT ##
+#######################
+
+cd /tmp
+sudo yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
+sudo systemctl enable amazon-ssm-agent
+sudo systemctl start amazon-ssm-agent
